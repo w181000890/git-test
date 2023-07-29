a413ee7fa0fa9aedd205e005ee07d0808212810a
信阳猛男 <45914987+w181000890@users.noreply.github.com>
2023-07-30 06:31:41 +0800

    服务器修改

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("main test");
         System.out.println("push hot-fix");
+        System.out.println("pull hot-fix");
     }
 }
