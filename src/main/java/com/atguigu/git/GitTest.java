8065bc537a04a08f841f15fdb67d57789cdc64d5
meng <s81000890@hnu.edu.cn>
2023-07-30 06:27:14 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("main test");
-        System.out.println("hot-fix");
+        System.out.println("push hot-fix");
     }
 }
